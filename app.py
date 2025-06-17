45b2bd03c08b73a8880e3a21dce5d5dd6802adb5
kauanhen02 <139480381+kauanhen02@users.noreply.github.com>
2025-06-17 11:35:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import oracledb
 app = Flask(__name__)
 PORT = 3000
 
+# Modo thin (sem Oracle Client instalado)
+oracledb.init_oracle_client()  # Opcional, só ativa se houver client
+
+# Rota principal
 @app.route("/produtos")
 def produtos():
     try:
@@ -16,11 +20,11 @@ def produtos():
 
         cursor = connection.cursor()
         cursor.execute("""
-            SELECT p.PRO_IN_CODIGO, p.PRO_ST_DESCRICAO, c.re_custo
-            FROM MEGA.gg_vw_composicao@GINGER c
-            INNER JOIN MEGA.gg_vw_produtos@GINGER p
-                ON c.PRO_IN_CODIGO = p.PRO_IN_CODIGO
-            WHERE LOWER(TO_CHAR(c.PRO_IN_CODIGO)) LIKE '%pr%'
+           SELECT p.PRO_IN_CODIGO, p.PRO_ST_DESCRICAO, c.re_custo
+    FROM MEGA.gg_vw_composicao@GINGER c
+    INNER JOIN MEGA.gg_vw_produtos@GINGER p
+        ON c.PRO_IN_CODIGO = p.PRO_IN_CODIGO
+    WHERE LOWER(c.PRO_IN_CODIGO) LIKE '%pr%'
         """)
 
         columns = [col[0] for col in cursor.description]
